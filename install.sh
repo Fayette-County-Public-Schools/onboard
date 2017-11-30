24dcd3530f7929a9f2a65737cc3eff270ac22cc8
Marcel Pociot <m.pociot@gmail.com>
2017-11-30 21:39:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
 # Install global Composer packages
-/usr/local/bin/composer global require laravel/installer laravel/lumen-installer laravel/spark-installer laravel/valet tightenco/jigsaw
+/usr/local/bin/composer global require laravel/installer laravel/lumen-installer laravel/spark-installer laravel/valet tightenco/jigsaw laravel/envoy botman/installer
 
 # Install Laravel Valet
 $HOME/.composer/vendor/bin/valet install
@@ -32,10 +32,7 @@ npm install --global yarn now
 
 # Create a Sites directory
 # This is a default directory for macOS user accounts but doesn't comes pre-installed
-mkdir $HOME/Sites
-
-# Symlink the Mackup config file to the home directory
-ln -s ./.mackup.cfg $HOME/.mackup.cfg
+mkdir $HOME/Code
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
